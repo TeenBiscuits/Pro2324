e845778dd41a6df2d1eb74148cb89de3c35dc271
Pablo Portas <81629707+TeenBiscuits@users.noreply.github.com>
2023-12-04 19:40:41 +0100

    Añadido Ejercicio_10 Bol_3

--- Boletin_3/Ejercicio_10.c
+++ Boletin_3/Ejercicio_10.c
@@ -8,8 +8,8 @@
  *       tipo entero y que devuelva la letra P si el número es positivo o N si el número es
  *       negativo. Además del subprograma, implemente la función apropiada para
  *       probar su funcionamiento, activando este último desde el programa principal.
- * in:
- * out:
+ * in: un número entero
+ * out: P si es positivo, N si es negativo
  * NOTA1:
  */
 
@@ -17,19 +17,26 @@
 #include <stdio.h>
 
 // Constantes
-
-
-// Definiciones
-
+#define PETICION "Introduzca un número entero: "
 
 // Definición funciones
-
+char esPosioNega(int entero);
 
 // Main
 int main() {
+    int entero;
 
+    printf(PETICION);
+    scanf("%d",&entero);
+    printf("%c",esPosioNega(entero));
 
     return 0;
 }
 
 // Declaración Funciones
+char esPosioNega(int entero){
+    char aux;
+    if (entero >= 0) aux = 'P';
+    else aux = 'N';
+    return aux;
+}
