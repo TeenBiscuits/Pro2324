649e000aa4ebd8fa7e19fa78edf46464411e9b5a
Fraagaa <66569329+Fraagaa@users.noreply.github.com>
2023-12-06 12:05:38 +0100

    Pull 1
    ## Boletín 2:
    - 18
    - 19
    - 20
    - 21
    - 38
    - 49
    ## Boletín 4
    - 01
    - 09

---
+++ src/Boletin_4/Ejercicio_09.c
@@ -0,0 +1,55 @@
+//
+// Created by Miguel Fraga on 04/12/2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+void escanearDatos(int *numeros,int tamano)
+{
+    int i;
+    for(i = 0 ;i < tamano; i++)
+    {
+        printf("Introduzca un valor:\n");
+        scanf("%d",&numeros[i]);
+    }
+}
+void imprimirDatos (int *numeros, int tamano){
+    int i;
+    for(i = 0 ;i < tamano; i++)
+    {
+        printf("%d, ",numeros[i]);
+    }
+}
+void modificarArray(int *numeros,int tamano){
+    int i;
+    for(i = 0; i < tamano; i++)
+    {
+        if(numeros[i]%2==0)
+        {
+            numeros[i]=pow(numeros[i],2);
+        }
+        else if(numeros[i] % 2 != 0)
+        {
+            numeros[i] = trunc(sqrt(abs(numeros[i])));
+        }
+    }
+}
+int main(){
+    int tamano;
+    printf("Introduzca cuantos numeros va a proprocionar:\n");
+    scanf("%d",&tamano);
+    if(tamano<=0 || tamano>20)
+    {
+        printf("Introduzca un tamano valido");
+        return 2;
+    }
+    int numeros[tamano];
+    escanearDatos(numeros,tamano);
+    imprimirDatos(numeros,tamano);
+    printf("\n");
+    modificarArray(numeros,tamano);
+    printf("\n");
+    imprimirDatos(numeros,tamano);
+    return 0;
+}
