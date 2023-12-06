a16543647259e925053887981cd295630b922245
sneiira <santiago.neisej@gmail.com>
2023-12-06 14:22:06 +0100

    Añadir ejers b2
    16,,22,23,24,25,26,27,29,30,31,32,33,34,35,36,37,40,41,42 y 44

---
+++ src/Boletin_2/Ejercicio_42.c
@@ -0,0 +1,21 @@
+//
+// Created by santi on 29/10/2023.
+//
+#include <stdio.h>
+int main(){
+    float dinero_euros;
+    float dinero_dolares;
+
+    printf("Introduzca la cantidad de euros que quiere cambiar:\n");
+    scanf("%f",&dinero_euros);
+
+    while(dinero_euros<0 || dinero_euros>10000000){
+        printf("Cantidad no valida introduzca una cantidad valida(0-10000000):\n");
+        scanf("%f",&dinero_euros);
+    }
+
+    dinero_dolares= dinero_euros * 1.06;
+    printf("%.2f euros son %.2f dolares",dinero_euros,dinero_dolares);
+
+    return 0;
+}
