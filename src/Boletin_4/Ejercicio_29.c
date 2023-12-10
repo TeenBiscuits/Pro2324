3893b61bb39eaa8eda8ad9d9547b4a3d1a4d005a
Fraagaa <66569329+Fraagaa@users.noreply.github.com>
2023-12-10 17:50:18 +0100

    Bol 4: 29

---
+++ src/Boletin_4/Ejercicio_29.c
@@ -0,0 +1,36 @@
+//
+// Created by Miguel Fraga on 10/12/2023.
+//
+
+/*
+ *  Diseñe una función que devuelva al revés la cadena recibida como parámetro
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void invertirCadena(char cadena[])
+{
+    int longitud = strlen(cadena);
+    int i, j;
+
+    for (i = 0, j = longitud - 1; i < j; i++, j--)
+    {
+        char temp = cadena[i];
+        cadena[i] = cadena[j];
+        cadena[j] = temp;
+    }
+}
+
+int main()
+{
+    printf("Introduce una oracion o frase\n");
+    char cadenaOriginal[50];
+    gets(&cadenaOriginal);
+
+    invertirCadena(cadenaOriginal);
+
+    printf("Cadena invertida: %s\n", cadenaOriginal);
+
+    return 0;
+}
