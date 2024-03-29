06b2282109285d18dd4f2d9450c8eb520cddeb63
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-03-29 20:51:36 +0100

    Combinados Cookie and GTag Files

--- docs/cfg/analytics.js
+++ docs/cfg/analytics.js
@@ -1,6 +1,21 @@
+<!--
+SPDX-FileCopyrightText: 2024 Pablo Portas López <pablo.portas@udc.es>
+
+SPDX-License-Identifier: GPL-3.0-only
+-->
+<!-- Cookie Policy -->
 <script type="text/javascript">
     var _iub = _iub || [];
     _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferencesButtonDisplay":"anchored-bottom-left","lang":"es","perPurposeConsent":true,"siteId":3576343,"whitelabel":false,"cookiePolicyId":31085815,"floatingPreferencesButtonCaption":true, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-top-center","rejectButtonDisplay":true,"showTitle":false }};
 </script>
 <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3576343.js"></script>
 <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-R2981R9F76"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R2981R9F76');
+</script>
