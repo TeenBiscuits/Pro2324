89f264d63f1e79c1444d96366b0ca9479d3ba4ef
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-04-04 19:56:59 +0200

    Update analytics.js

--- docs/cfg/analytics.js
+++ docs/cfg/analytics.js
@@ -6,7 +6,7 @@ SPDX-License-Identifier: GPL-3.0-only
 <!-- Cookie Policy -->
 <script type="text/javascript">
     var _iub = _iub || [];
-    _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferencesButtonDisplay":"anchored-bottom-left","lang":"es","perPurposeConsent":true,"siteId":3576343,"whitelabel":false,"cookiePolicyId":31085815,"floatingPreferencesButtonCaption":true, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"listPurposes":true,"position":"float-top-center","rejectButtonDisplay":true,"showTitle":false }};
+_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferencesButtonDisplay":"anchored-bottom-left","lang":"es","perPurposeConsent":true,"siteId":3576343,"whitelabel":false,"cookiePolicyId":31085815, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"listPurposes":true,"position":"float-bottom-right","rejectButtonDisplay":true,"showTitle":false }};
 </script>
 <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3576343.js"></script>
 <script type="text/javascript" src="//cdn.iubenda.com/cs/beta/iubenda_cs.js" charset="UTF-8" async></script>
