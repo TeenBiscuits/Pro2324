f5ac0857a38aa2e4c763543d197bedc99f3df441
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-05-06 12:03:14 +0200

    Typo CopyList

--- docs/code/Ejemplos/Tema_3/copyList.c
+++ docs/code/Ejemplos/Tema_3/copyList.c
@@ -2,18 +2,18 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
-bool copyList(tlist L, tList *M) {
+bool copyList(tlist listaDest, tList *listaOrig) {
     tPosl p, q, r;
     bool ret = true;
 
-    createEmptyList(M);
-    if (!isEmptyList(L)) {
-        p = L;
+    createEmptyList(listaOrig);
+    if (!isEmptyList(listaDest)) {
+        p = listaDest;
         while ((p != LNULL) && createNode(&r)) {
             r->data = p->data;
             r->next = LNULL;
-            if (p == L) {
-                *M = r;
+            if (p == listaDest) {
+                *listaOrig = r;
                 q = r;
             } else {
                 q->next = r;
@@ -22,7 +22,7 @@ bool copyList(tlist L, tList *M) {
             p = p->next;
         }
         if (p != LNULL) {
-            deleteList(M);
+            deleteList(listaOrig);
             ret = false;
         }
     }
