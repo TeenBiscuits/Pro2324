0f02d5522242357812041d6c6121c90f07f51078
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-10-12 14:04:40 +0200

    Actualizada la gtag al nuevo dominio

--- docs/cfg/analytics.js
+++ docs/cfg/analytics.js
@@ -6,11 +6,11 @@ _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferenc
 <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3576343.js"></script>
 <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R2981R9F76"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MK6BSW78PJ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-R2981R9F76');
+  gtag('config', 'G-MK6BSW78PJ');
 </script>
